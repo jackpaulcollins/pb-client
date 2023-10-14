a2209999d0b7577b7dccd052a580b1348a4771d3
Jack Collins <jackpaulcollins@gmail.com>
2023-10-14 09:28:14 -0700

    change default profile pic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { useRouter } from 'next/navigation';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useAuthContext } from '@/app/contexts/AuthProvider';
+import DefaultUserPicture from './assets/icons/DefaultUserPicture';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -83,11 +84,7 @@ function Navbar() {
                         <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                           <span className="absolute -inset-1.5" />
                           <span className="sr-only">Open user menu</span>
-                          <img
-                            className="h-8 w-8 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
-                          />
+                          <DefaultUserPicture />
                         </Menu.Button>
                       </div>
                       <Transition
@@ -187,11 +184,7 @@ function Navbar() {
               <div className="border-t border-gray-700 pb-3 pt-4">
                 <div className="flex items-center px-5">
                   <div className="flex-shrink-0">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
+                    <DefaultUserPicture />
                   </div>
                   <div className="ml-3">
                     <div className="text-base font-medium text-white">
